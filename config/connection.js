c8ad21e33013a428e1b6659fe2e85ba14a86354f
Lisa Rozo <erozo190@gmail.com>
2021-11-06 11:13:07 -0500

    added associations

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,21 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+: new Sequelize('ecommerce_DB', 'root', 'Themoon01', {
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
 module.exports = sequelize;
